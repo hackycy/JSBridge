cdee8ab768f0c3f0f4c75191932ca0ea13c7a103
hackycy <qa894178522@qq.com>
2020-12-05 01:28:38 +0800

    Release 0.0.7（Beta）

--- dist/jsbridge.js
+++ dist/jsbridge.js
@@ -1,14 +1,5 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else if(typeof exports === 'object')
-		exports["JSBridge"] = factory();
-	else
-		root["JSBridge"] = factory();
-})(self, function() {
-return /******/ (() => { // webpackBootstrap
+window.JSBridge =
+/******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
@@ -39,35 +30,28 @@ var invokeCallbacks = {};
 var handleRegisterCallbacks = {};
 var invokeCallbackId = 0;
 
+/**
+ * 用于判断环境是否可用jsbridge
+ */
 function hasBridge() {
   try {
     if (!isWebView) {
       return false;
     }
     if (isIosWebView) {
-      return window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.invokeHandler ? true : false;
+      return window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers._invokeHandler ? true : false;
     }
-    return window.invokeHandler ? true : false;
+    return window._invokeHandler ? true : false;
   } catch (e) {
     return false;
   }
 }
 
-function invokeHandler(func, paramsString, callbackId) {
-  if (isIosWebView) {
-    // iOS
-    window.webkit.messageHandlers.invokeHandler.postMessage({
-      invoke: func,
-      paramsString: paramsString,
-      callbackId: callbackId
-    });
-  } else {
-    // Android
-    window.invokeHandler.invoke(func, paramsString, callbackId);
-  }
-}
-
-// JS主动调用原生方法后的回调处理
+/**
+ * JS主动调用原生方法后的回调处理
+ * @param {Number} callbackId 自增的callbackId
+ * @param {Object} execResult 执行结果
+ */
 function handleInvokeCallbackFromNative(callbackId, execResult) {
   if (typeof invokeCallbacks[callbackId] === 'function') {
     try {
@@ -81,7 +65,12 @@ function handleInvokeCallbackFromNative(callbackId, execResult) {
   delete invokeCallbacks[callbackId];
 }
 
-// 原生主动调用JS方法
+
+/**
+ * 原生主动调用JS方法
+ * @param {String} func unique name
+ * @param {String} paramString 参数
+ */
 function handleMessageFromNative(func, paramString) {
   var param;
   try {
@@ -92,7 +81,32 @@ function handleMessageFromNative(func, paramString) {
   return handleRegisterCallbacks[func](param);
 }
 
-// JS主动调用原生方法
+/**
+ * real invoke
+ * @param {String} func unique name
+ * @param {String} paramsString 参数
+ * @param {Number} callbackId 自增的callbackId
+ */
+function invokeHandler(func, paramsString, callbackId) {
+  if (isIosWebView) {
+    // iOS
+    window.webkit.messageHandlers._invokeHandler.postMessage({
+      invoke: func,
+      paramsString: paramsString,
+      callbackId: callbackId
+    });
+  } else {
+    // Android
+    window._invokeHandler.invoke(func, paramsString, callbackId);
+  }
+}
+
+/**
+ * JS主动调用原生方法
+ * @param {String} func unique name
+ * @param {Object} params object
+ * @param {Function} callback 回调函数
+ */
 function invoke(func, params, callback) {
   if (!hasBridge()) {
     return;
@@ -109,7 +123,52 @@ function invoke(func, params, callback) {
   invokeHandler(func, paramsString, callbackId);
 }
 
-// 注册函数，等待原生主动调用
+/**
+ * real sync invoke
+ * @param {String} func unique name
+ * @param {String} paramsString 参数
+ * @param {Number} callbackId 自增的callbackId
+ */
+function invokeSyncHandler(func, paramsString, callbackId) {
+  if (isIosWebView) {
+    // TODO
+  } else {
+    var syncResult = window._invokeHandler.invokeSync(func, paramsString, callbackId);
+    try {
+      return JSON.parse(syncResult)
+    } catch(e) {
+      return syncResult;
+    }
+  }
+}
+
+/**
+ * JS主动调用原生方法，同步返回
+ * @param {String} func unique name
+ * @param {Object} params object
+ * @param {Function} callback 回调函数
+ */
+function invokeSync(func, params, callback) {
+  if (!hasBridge()) {
+    return;
+  }
+  if (!func || typeof func !== 'string') {
+    return;
+  }
+  if (!params || typeof params !== 'object') {
+    params = {};
+  }
+  var paramsString = JSON.stringify(params);
+  var callbackId = ++invokeCallbackId;
+  invokeCallbacks[callbackId] = callback;
+  invokeSyncHandler(func, paramsString, callbackId);
+}
+
+/**
+ * 注册函数，等待原生主动调用
+ * @param {String} func unique name
+ * @param {Function} executor 执行函数
+ */
 function register(func, executor) {
   if (!hasBridge()) {
     return;
@@ -120,7 +179,7 @@ function register(func, executor) {
   handleRegisterCallbacks[func] = executor;
 }
 
-// mount attr
+// 挂载给Native端进行执行
 window._handleMessageFromNative = handleMessageFromNative;
 window._handleInvokeCallbackFromNative = handleInvokeCallbackFromNative;
 
@@ -130,7 +189,8 @@ if (hasBridge()) {
 }
 
 module.exports = {
-  invoke: invoke,
+  invoke,
+  invokeSync,
   register: register
 };
 
@@ -169,5 +229,4 @@ module.exports = {
 /******/ 	return __webpack_require__(10);
 /******/ })()
 ;
-});
 //# sourceMappingURL=jsbridge.map
