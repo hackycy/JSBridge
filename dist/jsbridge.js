8ce17008461a12511cedab5f42ef43feeaa41f12
hackycy <qa894178522@qq.com>
2020-12-05 18:41:38 +0800

    Release 0.0.9（beta）

--- dist/jsbridge.js
+++ dist/jsbridge.js
@@ -1,5 +1,14 @@
-window.JSBridge =
-/******/ (() => { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["JSBridge"] = factory();
+	else
+		root["JSBridge"] = factory();
+})(self, function() {
+return /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
@@ -228,4 +237,5 @@ module.exports = {
 /******/ 	return __webpack_require__(10);
 /******/ })()
 ;
+});
 //# sourceMappingURL=jsbridge.map
